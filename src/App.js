2fe46537bbdea9d2ce1b659752ea2b2e404242e4
thegreytangent <thegreytangent@gmail.com>
2022-12-17 01:04:30 +0800

    Feature: add data using dispatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { addUser } from './features/Users';
+import { useState } from 'react';
 
 function App() {
+
+  const dispatch =  useDispatch();
+  const userList = useSelector((state) => state.users.value );
+
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  
+
+  const onChangeName = (e) => {
+      setName(e.target.value);
+  }
+
+
+
+// console.log("--- ", userList);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='addUser'>
+        {username}
+        <input type="text" placeholder="Name..." onChange={onChangeName}/>
+        <input type="text" placeholder="Username..." 
+        onChange={(event) => { 
+              setUsername(event.target.value)}
+            } />
+        <button onClick={() => {dispatch(addUser({
+          id: 2333,
+          name: name,
+          username: username
+        }))}}>Add User</button>
+      </div>
+      <div className='displayUsers'>
+      {userList.map((user, i) => {
+        return <div key={i}>
+          <h1>Name: {user.name}</h1>
+          <h1>Username: {user.username}</h1>
+        </div>
+      })}
+      </div>
     </div>
   );
 }
---
+++ src/FakeData.js
@@ -0,0 +1,12 @@
+export const UsersData = [
+    {
+        id: 1,
+        name: "test",
+        username: "test1"
+    },
+    {
+        id: 2,
+        name: "breat",
+        username: "breat1"
+    }
+]
